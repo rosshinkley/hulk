43dba7efb9703946e49fb9885fccd1f83b8c48de
Ross <ross@treasurylane.com>
2015-09-23 19:36:31 -0400

    promisifying config

--- config.js
+++ config.js
@@ -1,23 +1,24 @@
-var fs = require('fs');
-var path = require('path');
-var yaml = require('yaml-js');
-var _ = require('lodash');
+var Promise = require('bluebird'),
+    fs = Promise.promisifyAll(require('fs')),
+    path = require('path'),
+    yaml = require('yaml-js'),
+    _ = require('lodash'),
 
-// read config
-var defaults = {
-    source     : process.cwd(),
-    destination: '_site',
-    layouts    : '_layouts',
-    posts      : '_posts',
-    permalink  : '/{{year}}/{{month}}/{{day}}/{{slug}}.html',
-    ignore     : [
-        '_config.yml',
-        'node_modules/**',
-        'package.json',
-        '**/.*', // dot files
-        '.*/**' // dot files
-    ]
-};
+    // read config
+    defaults = {
+        source: process.cwd(),
+        destination: '_site',
+        layouts: '_layouts',
+        posts: '_posts',
+        permalink: '/{{year}}/{{month}}/{{day}}/{{slug}}.html',
+        ignore: [
+            '_config.yml',
+            'node_modules/**',
+            'package.json',
+            '**/.*', // dot files
+            '.*/**' // dot files
+        ]
+    };
 
 function ignoreSpecialFolders(config) {
     var ignores = [
@@ -31,37 +32,30 @@ function ignoreSpecialFolders(config) {
     }
 }
 
-function readConfig(callback) {
+function readConfig() {
     var configPath = path.join(process.cwd(), '_config.yml');
     console.log('Configuration from %s', configPath);
+    return fs.readFileAsync(configPath, 'utf8')
+        .then(function(data) {
+            var config = yaml.load(data);
 
-    fs.readFile(configPath, 'utf8', function (err, data) {
-        if (err) {
-            callback(err);
-            return;
-        }
+            // Override the default configuration values with the site's config.
+            config = _.defaults(config, defaults);
 
-        var config = yaml.load(data);
+            // Merge the config file's ignore list with our default ignores.
+            // Unlike other configuration options the user can't override
+            // our defaults, only append to them.
+            config.ignore = _.union(config.ignore, defaults.ignore);
 
-        // Override the default configuration values with the site's config.
-        config = _.defaults(config, defaults);
+            // Trim trailing forward-slash from the URL.
+            // This is only applicable to relative URLs of course.
+            // All page and post URLs will begin with a forward-slash
+            // and this prevents doubling up.
+            config.url = config.url.replace(/\/$/, '');
 
-        // Merge the config file's ignore list with our default ignores.
-        // Unlike other configuration options the user can't override
-        // our defaults, only append to them.
-        config.ignore = _.union(config.ignore, defaults.ignore);
-
-        // Trim trailing forward-slash from the URL.
-        // This is only applicable to relative URLs of course.
-        // All page and post URLs will begin with a forward-slash
-        // and this prevents doubling up.
-        config.url = config.url.replace(/\/$/, '');
-
-        // Ignore some special folders set in the configuration.
-        ignoreSpecialFolders(config);
-
-        callback(null, config);
-    });
+            // Ignore some special folders set in the configuration.
+            ignoreSpecialFolders(config);
+        });
 }
 
 module.exports = {
